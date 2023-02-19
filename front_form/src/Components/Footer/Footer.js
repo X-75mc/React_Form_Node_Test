ec5dcefa12d3165e57b15d5d2b57325fc11ceed5
Artem Osypenko <artoss1234@gmail.com>
2023-02-19 13:29:17 +0200

    commit 7

--- front_form/src/Components/Footer/Footer.js
+++ front_form/src/Components/Footer/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './footer.css';
+import socialGrup from './img/social_grup.png';
+import sunBig from './img/Sun_big_footer.png';
+import sunGreen from './img/sun_green_footer.png';
+import sunBlue from './img/sun_blue_footer.png';
 
 const Footer = () => {
     return (
-        <></>
+        <>
+            <div className="Bg_footer">
+                <div className="footer">
+                    <div className="social_block">
+                        <img className="social_list" src={socialGrup} alt="" />
+                        <img className="sunBig" src={sunBig} alt="" />
+                        <img className="sunGreen" src={sunGreen} alt="" />
+                        <img className="sunBlue" src={sunBlue} alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
--- front_form/src/Components/Header/Header.js
+++ front_form/src/Components/Header/Header.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import vector1 from './img/Vector_1.png';
+import Captura from './img/Capture.png';
+import sunSmallEllow from './img/sun_small_ellow.png';
+import eclipseEllow from './img/Ellipse_ellow.png';
+import sunBigFiollet from './img/sun_big_fiolet.png';
+import sunEllowRot from './img/sun_ellow_rot.png';
+import './style.css';
 
 
 const Header = () => {
 
     const [data, setData] = useState('');
+    
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -28,17 +36,28 @@ const Header = () => {
 
     return (
         <header>
-            <div className='container'>
-                <div className='bg'>
+            <div className="container">
+                <div className="bg">
+                  <img className="vector_1" src={vector1} alt="Vector" />
+                  <img className="vector_2" src={Captura} alt="Captura" />
+                  <img className="vector_3" src={sunSmallEllow} alt="Vector" />
+                  <img  className="vector_4" src={eclipseEllow} alt="" />
+                  <img  className="vector_5" src={sunBigFiollet} alt="" />
+                  <img className="vector_6" src={sunEllowRot} alt="" />
+                  <div className="form_div">
                     <form onSubmit={handleSubmit}>
-                        <input type="text" name="name" />
-                        <input type="text" name="mail" />
-                        <textarea name="message"></textarea>
-                        <button type="submit">Submit</button>
-                    </form>
+                        <h3 className='text_top'>Reach out to us!</h3>
+                        <input className="inp_text_name" type="text" name="name"   />
+                        <input className="inp_text_mail" type="text" name="mail" />
+                        <textarea className="textarea" name="message"></textarea>
+                        <button className="btn" type="submit">Message</button>
+                      </form>
+                  </div>
+                    
                 </div>
             </div>
         </header>
+      
     );
 }
 
